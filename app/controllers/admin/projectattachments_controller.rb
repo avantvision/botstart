e5a1b5ed123302e3ec123fac29e92a5595627820
Carlos Barbosa <sistemas@avantvision.net>
2009-02-22 14:11:57 -0600

    File Uploads Better

--- app/controllers/admin/projectattachments_controller.rb
+++ app/controllers/admin/projectattachments_controller.rb
@@ -3,5 +3,6 @@ class Admin::ProjectattachmentsController < Admin::AdminSiteController
   hobo_model_controller Projectattachment
 
   auto_actions :all
+  auto_actions_for :project, [:create, :new]
 
 end
--- app/controllers/admin/tasks_controller.rb
+++ app/controllers/admin/tasks_controller.rb
@@ -3,5 +3,5 @@ class Admin::TasksController < Admin::AdminSiteController
   hobo_model_controller Task
 
   auto_actions :all
-
+  auto_actions_for :project, [:create, :new]
 end
