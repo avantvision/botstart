a6c148b0914049d15596c15f19b86fd81402246d
Carlos Barbosa <sistemas@avantvision.net>
2009-02-25 22:11:43 -0600

    going to try writeboards

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
 
   hobo_model_controller
-  auto_actions :edit, :update, :destroy , :reorder
+  auto_actions :edit, :update, :destroy , :reorder, :feed
   auto_actions_for :user, [:create, :new]
   auto_actions_for :project, [:create, :index, :new]
 
-
-
+  def feed
+     @tasks = Task.find(:all, :limit => 10, :order => "position")
+   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -16,6 +16,7 @@ class Task < ActiveRecord::Base
  validates_presence_of :name
   
    
+ 
 
 
   # --- Permissions --- #
